0a971c491b364edd9268f96e6a53472d4c71780f
Cem Aydin <cem.aydin@gmx.ch>
2018-09-19 17:47:19 +0200

    improved version output, corr. v. in xml

--- kivishop/kivishop/version.php
+++ kivishop/kivishop/version.php
@@ -6,9 +6,26 @@ class KivishopApiResourceVersion extends ApiResource
 {
     public function get()
     {
+        // get com_api version
+        // -> TODO
+
+        // get this plugins version
+        $plg_xml_file = JPATH_SITE . '/plugins/api/kivishop/kivishop.xml';
+        if (file_exists($plg_xml_file)) {
+            $plg_xml = simplexml_load_file($plg_xml_file);
+            $plg_ver = (string)$plg_xml->version;
+        } else {
+            $plg_ver = "UNKNOWN";
+        }
+        // debug
+        //var_dump($plg_xml);
+
+        // set results
         $result = new \stdClass;
-        $result->version = 0;
-        $result->revision = 1;
+        $result->joomla_ver = JVERSION;
+        $result->com_api_ver = "";
+        $result->kivishop_plg_ver = $plg_ver;
+
         $this->plugin->setResponse( $result );
     }
 }
